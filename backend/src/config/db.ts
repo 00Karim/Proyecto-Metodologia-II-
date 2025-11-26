86c21199fa64f6b477ec1a944000b47a4aa5b940
franbaudrix <franco39239@gmail.com>
2025-11-26 17:45:10 -0300

    Implementacion del patron Singleton en la conexion de la base de datos

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 
+let isConnected = false; //Variable booleana para comprobar si se establecio una conexion a mongoDB o no
+
 export const connectDB = async() => {
+
+    if(isConnected){
+        console.log("MongoDB conectado previamente")
+        return
+    }
+
     try {
-        await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/TripMate");
+        const db = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/TripMate");
+        isConnected = db.connections[0]?.readyState === 1 //Esta linea comprueba que se haya establecido correctamente la conexion a la base de datos y no haya un falso positivo 
         console.log("MongoDB Conectado correctamente");
     } catch(err){
         console.error("MongoDB no ha sido conectado correctamente", err);
         process.exit(1);
     }
-}; // TODO: Implementar singleton
+}; 
